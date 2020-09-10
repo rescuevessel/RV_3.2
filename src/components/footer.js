12676a121a2740ae4ccd0002e0fd7c95d2f58ec9
rescuevessel <66702285+rescuevessel@users.noreply.github.com>
2020-09-10 14:06:38 -0400

    footer - fingers crossed

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,9 +2,8 @@ import React from "react"
 import { Link } from "gatsby"
 //import moment from 'moment-timezone'
 import "./layout.scss"
-import "./footer-new.scss"
+import "./footer.scss"
 import logo from "../images/rv_icon.png"
-
 
 const Footer = class extends React.Component {
   render() {
