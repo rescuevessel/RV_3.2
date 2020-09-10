1089c6eea76fb2126d1ef44db6580dff9fa35a42
rescuevessel <66702285+rescuevessel@users.noreply.github.com>
2020-09-10 01:25:53 -0400

    had to fix something

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,35 +17,35 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
-        start_url: '/',
-        display: 'standalone',
-        icon: 'src/images/favicon.png',
+        start_url: "/",
+        display: "standalone",
+        icon: "src/images/favicon.png",
       },
-    },  
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [],
+      },
+    },
     {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: `i5mxz0zh67lx`,
         // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken: '1kR_mZyBhpsxpr769RRBkfTeYjsFf42ZjPyz334ZEkI',
+        accessToken: "1kR_mZyBhpsxpr769RRBkfTeYjsFf42ZjPyz334ZEkI",
       },
     },
     {
       resolve: `gatsby-source-dropbox`,
       options: {
         accessToken: `9OB_98L2OKEAAAAAAABqFmrGZRoQJVmxLYeuozKKEbf4pBK_jr0HtD5cAB8sIQPI`,
-        extensions: ['.pdf', '.jpg', '.png', '.gif', '.md'],
-        path: '',
+        extensions: [".pdf", ".jpg", ".png", ".gif", ".md"],
+        path: "",
         recursive: false,
         createFolderNodes: false,
-      }
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [],
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/feed.js
+++ src/pages/feed.js
@@ -13,12 +13,12 @@ export default function FeedPage({ data }) {
             <h1>Feed me!</h1>
 
             {data.allDropboxNode.edges.map(({ node }, index) => {
-              const iframe = node.localFile.childMarkdownRemark.frontmatter
-
               let num = Math.floor(Math.random() * 800) + 1 //random value 0-800
               num *= Math.floor(Math.random() * 2) === 1 ? 1 : -1 //include negative numbers
 
               if (node.localFile.extension === "md") {
+                const iframe = node.localFile.childMarkdownRemark.frontmatter
+                console.log(iframe)
                 return (
                   <li className="feed-item" style={{ marginLeft: num }}>
                     <iframe
@@ -55,15 +55,14 @@ export const feedquery = graphql`
     allDropboxNode {
       edges {
         node {
-          id
           localFile {
+            extension
             childMarkdownRemark {
               frontmatter {
-                templateKey
                 content
+                templateKey
               }
             }
-            extension
             publicURL
           }
         }
