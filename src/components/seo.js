14ac0c7c01e438d53d26c1735d4587c7469865dd
rescuevessel <dave@rescuevessel.com>
2020-12-08 18:03:45 -0500

    more google

--- src/components/seo.js
+++ src/components/seo.js
@@ -5,7 +5,10 @@ import { useStaticQuery, graphql } from 'gatsby'
 import ReactGA from 'react-ga'
 
 ReactGA.initialize('UA-81279213-1')
-ReactGA.pageview('/', '/about', '/work', '/feed')
+ReactGA.pageview('/')
+ReactGA.pageview('/about')
+ReactGA.pageview('/work')
+ReactGA.pageview('/feed')
 
 // <!-- Global site tag (gtag.js) - Google Analytics -->
 // <script async src="https://www.googletagmanager.com/gtag/js?id=UA-81279213-1"></script>
